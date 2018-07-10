02cfaf8a2fcd1086534f879799cc79709e3c047c
HeRoMo <hero@asterisk-works.jp>
2018-07-10 23:29:13 +0900

    add HelpIntentHandler

--- index.ts
+++ index.ts
@@ -3,12 +3,14 @@ import { Handler } from "aws-lambda";
 
 import ErrorHandler from "./src/handlers/ErrorHandler";
 import HelloIntentHandler from "./src/handlers/HelloIntentHandler";
+import HelpIntentHandler from "./src/handlers/HelpIntentHandler";
 import LaunchRequestHandler from "./src/handlers/LaunchRequestHandler";
 
 export const handler: Handler = SkillBuilders.standard()
     .addRequestHandlers(
       LaunchRequestHandler,
       HelloIntentHandler,
+      HelpIntentHandler,
     )
     .addErrorHandlers(ErrorHandler)
     .lambda();
